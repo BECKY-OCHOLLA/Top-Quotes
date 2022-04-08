ea8f02962661b10a1c892f001aa7d995976d4365
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-09 01:48:36 +0300

    it will add quotes and an array to the quote component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quote:Quote[]=[
+    new Quote(1,'Visions are worth fighting for.Why spend your life making someone else dreams?-Tim Burton',0,0),
+    new Quote(2,'Hey bug! Manically packing up your office in a cold sweat. What is with the downward spiral honey?— Walter the Lovebug (voice)',0,0),
+    new Quote(3,'Be gentle with yourself,you are doing the best you can',0,0)
+
+  ];
 
   constructor() { }
 
