86800f9d80004e4aab43287e296001edcd9efa05
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-09 18:21:10 +0300

    it will add time,pipes to the project

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,9 @@
+import { Time } from "@angular/common";
+
 export class Quote {
     
     showQuote:boolean;
-     constructor(public id:number,public author:string, public quote:string,public upvote:number,public downvote:number){
+     constructor(public id:number,public author:string, public quote:string,public upvote:number,public downvote:number,public completeDate: Date,){
          this.showQuote=false;
 
     }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { time } from 'console';
 import{Quote} from '../quote';
 
 @Component({
@@ -8,9 +9,9 @@ import{Quote} from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quote:Quote[]=[
-    new Quote(1,'By-Oprah Winfrey','If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough. ',0,0),
-    new Quote(2,'By— Walter the Lovebug (voice)','Hey bug! Manically packing up your office in a cold sweat. What is with the downward spiral honey?',0,0),
-    new Quote(3,'By-Steve Jobs','Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other people thinking. ',0,0)
+    new Quote(1,'By-Oprah Winfrey','If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough. ',0,0,new Date(2020, 8 ,8)),
+    new Quote(2,'By— Walter the Lovebug (voice)','Hey bug! Manically packing up your office in a cold sweat. What is with the downward spiral honey?',0,0,new Date(2022,1,16)),
+    new Quote(3,'By-Steve Jobs','Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other people thinking. ',0,0,new Date(2021,5,5))
 
   ];
   toggleQuote(index){
